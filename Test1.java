8786793d2e4fe5a8e6d95aeacae1ac2a51109bbd
DESKTOP-JJG4219\Praveen Reddy <kuntapraveenreddy90@gmail.com>
2018-01-28 20:04:08 -0600

    new files added

---
+++ Test1.java
@@ -0,0 +1,9 @@
+class Test 
+{
+	static int a=1000,b=2000;
+		public static void main(String[] args)
+	{
+		System.out.println(Test.a);
+		System.out.println(Test.b);
+	}
+}
---
+++ Test2.java
@@ -0,0 +1,11 @@
+class Test2
+{
+	static int c=100;
+	public static void main(String[] args)
+	{
+		System.out.println(a);
+		System.out.println(Test2.c);
+		Test t2 = new Test();
+		System.out.println(t2.c);
+	}
+}
